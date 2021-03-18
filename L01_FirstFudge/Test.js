8f7e0a298d8ccf923284c8704e5990b839cde79a
Lennard <lennard.johannes.hurst@hs-furtwangen.de>
2021-03-18 16:15:02 +0100

    First Setup

---
+++ L01_FirstFudge/Test.js
@@ -0,0 +1,8 @@
+"use strict";
+var L01_FirstFudge;
+(function (L01_FirstFudge) {
+    var f = FudgeCore;
+    let node = new f.Node("Test");
+    console.log(node);
+})(L01_FirstFudge || (L01_FirstFudge = {}));
+//# sourceMappingURL=Test.js.map
---
+++ L01_FirstFudge/Test.ts
@@ -0,0 +1,8 @@
+namespace L01_FirstFudge {
+
+    import f = FudgeCore;
+
+    let node: f.Node = new f.Node("Test");
+
+    console.log(node);
+}
