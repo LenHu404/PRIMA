68a5fb91d1807118a731aa6f8de7fe729bc7980a
Lennard <lennard.johannes.hurst@hs-furtwangen.de>
2021-03-25 16:22:55 +0100

    First Fudge Finisehd

--- L01_FirstFudge/Test.js
+++ L01_FirstFudge/Test.js
@@ -2,7 +2,33 @@
 var L01_FirstFudge;
 (function (L01_FirstFudge) {
     var f = FudgeCore;
-    let node = new f.Node("Test");
-    console.log(node);
+    window.addEventListener("load", init);
+    function init(_event) {
+        let node = new f.Node("Test");
+        const canvas = document.querySelector("canvas");
+        let mesh = new f.MeshQuad("Quad");
+        let cmpMesh = new f.ComponentMesh(mesh);
+        cmpMesh.mtxPivot.rotateY(25);
+        node.addComponent(cmpMesh);
+        let material = new f.Material("Florian", f.ShaderUniColor, new f.CoatColored(new f.Color(1, 1, 1, 1)));
+        let cmpMaterial = new f.ComponentMaterial(material);
+        node.addComponent(cmpMaterial);
+        /* f.Color.CSS("'ff000", 1); */
+        let cmpCamera = new f.ComponentCamera();
+        cmpCamera.mtxPivot.translateZ(3);
+        cmpCamera.mtxPivot.rotateY(180);
+        let viewport = new f.Viewport();
+        viewport.initialize("Viewport", node, cmpCamera, canvas);
+        viewport.draw();
+        console.log(cmpCamera);
+        // rotateQuad(node, cmpMesh);
+    }
+    /* function rotateQuad(_node: f.Node, _cmpMesh: f.ComponentMesh): void {
+        while (true) {
+            _cmpMesh.mtxPivot.rotateY(25);
+            _node.addComponent(_cmpMesh);
+        }
+
+    } */
 })(L01_FirstFudge || (L01_FirstFudge = {}));
 //# sourceMappingURL=Test.js.map
--- L01_FirstFudge/Test.ts
+++ L01_FirstFudge/Test.ts
@@ -2,7 +2,50 @@ namespace L01_FirstFudge {
 
     import f = FudgeCore;
 
-    let node: f.Node = new f.Node("Test");
+    window.addEventListener("load", init);
 
-    console.log(node);
+    function init(_event: Event): void {
+
+        let node: f.Node = new f.Node("Test");
+
+        const canvas: HTMLCanvasElement = document.querySelector("canvas");
+
+        let mesh: f.MeshQuad = new f.MeshQuad("Quad");
+        let cmpMesh: f.ComponentMesh = new f.ComponentMesh(mesh);
+        cmpMesh.mtxPivot.rotateY(25);
+        node.addComponent(cmpMesh);
+
+
+        let material: f.Material = new f.Material("Florian", f.ShaderUniColor, new f.CoatColored(new f.Color(1, 1, 1, 1)));
+        let cmpMaterial: f.ComponentMaterial = new f.ComponentMaterial(material);
+        node.addComponent(cmpMaterial);
+
+        /* f.Color.CSS("'ff000", 1); */
+
+
+        let cmpCamera: f.ComponentCamera = new f.ComponentCamera();
+        cmpCamera.mtxPivot.translateZ(3);
+        cmpCamera.mtxPivot.rotateY(180);
+
+
+        let viewport: f.Viewport = new f.Viewport();
+        viewport.initialize("Viewport", node, cmpCamera, canvas);
+        viewport.draw();
+
+        console.log(cmpCamera);
+
+        // rotateQuad(node, cmpMesh);
+
+
+    }
+
+    /* function rotateQuad(_node: f.Node, _cmpMesh: f.ComponentMesh): void {
+        while (true) {
+            _cmpMesh.mtxPivot.rotateY(25);
+            _node.addComponent(_cmpMesh);
+        }
+
+    } */
+
+
 }
